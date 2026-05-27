593c6919df81c7b561b9cd8d6cb749b77326ba4f
Parv Tiwari <parvtiwari1@gmail.com>
2026-05-27 17:00:48 +0530

    Polish platform integration: edge-to-edge + iOS perm strings

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/map_screen.dart';
 import 'theme/tactical_theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarContrastEnforced: false,
+  ));
   runApp(const FalconMapApp());
 }
 
